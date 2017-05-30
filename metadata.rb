e9b897e06f22744f666f4125204c0b6a679a36fa
Tyler Wong <ty-w@live.com>
2017-05-29 19:04:26 -0700

    Bump cookbook version and add chef_version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,8 @@ maintainer_email 'ty-w@live.com'
 license 'Apache 2.0'
 description 'Installs/Configures SourceTree'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '1.0.1'
+version '1.1.0'
+chef_version '>= 12.1' if respond_to?(:chef_version)
 supports 'windows'
 issues_url 'https://github.com/T-Wong/sourcetree/issues' if respond_to?(:issues_url)
 source_url 'https://github.com/T-Wong/sourcetree' if respond_to?(:source_url)
