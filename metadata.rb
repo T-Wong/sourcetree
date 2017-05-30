abfd255c5dfab0a1489cb2dc766234e8faef9db8
Tyler Wong <ty-w@live.com>
2017-05-29 19:06:15 -0700

    Modify license to standardized license name

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name 'sourcetree'
 maintainer 'Tyler Wong'
 maintainer_email 'ty-w@live.com'
-license 'Apache 2.0'
+license 'Apache-2.0'
 description 'Installs/Configures SourceTree'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '1.1.0'
